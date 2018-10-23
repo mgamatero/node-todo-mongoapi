9b773da4dc38ce9d46f7124be01109678991760c
Michael Gamatero <mgamatero@gmail.com>
2018-10-22 19:28:54 -0700

    get route for all todos added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+//contains express and bodyparser, routes, app.listen
+//requires mongoose, Todo and User from db and models
 
-var {mongoose} = require('./db/mongoose')
-var {Todo} = require('./models/todo')
-var {User} = require('./models/user')
+
+var { mongoose } = require('./db/mongoose')
+var { Todo } = require('./models/todo')
+var { User } = require('./models/user')
 
 var express = require('express')
 var bodyParser = require('body-parser')
@@ -10,19 +13,29 @@ var app = express()
 
 app.use(bodyParser.json())
 
-app.post('/todos', (req,res) =>{
-    // console.log('Post: ',req.body)
-    var todo = new Todo ({
-        text:req.body.text
+app.post('/todos', (req, res) => {
+
+    var todo = new Todo({
+        text: req.body.text
     })
-    todo.save().then((doc)=>
-    res.send(doc))
-},(e)=>{
+    todo.save().then((doc) =>
+        res.send(doc))
+}, (e) => {
     res.status(400).send()
 })
 
 
-app.listen(3000, ()=>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos })
+    }, (e) => {
+        res.status(400).send()
+    })
+})
+
+
+
+app.listen(3000, () => {
     console.log('Listening on port 3000')
 })
 
