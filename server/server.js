0d4624743cc0708763e0b190e15e87814ca30689
Michael Gamatero <mgamatero@gmail.com>
2018-10-22 19:04:52 -0700

    initial commit, working refactored mongodb w/ express routes

---
+++ server/server.js
@@ -0,0 +1,53 @@
+
+var {mongoose} = require('./db/mongoose')
+var {Todo} = require('./models/todo')
+var {User} = require('./models/user')
+
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req,res) =>{
+    // console.log('Post: ',req.body)
+    var todo = new Todo ({
+        text:req.body.text
+    })
+    todo.save().then((doc)=>
+    res.send(doc))
+},(e)=>{
+    res.status(400).send()
+})
+
+
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000')
+})
+
+// var newTodo = new Todo({
+//     text: '  Edit This Video   ',
+//     completed: true,
+//     completedAt: 9999
+// })
+
+// newTodo.save().then((doc)=>{
+//     console.log('Saved todo: ', doc)
+// },(e)=>{
+//     console.log('Unable to save')
+// });
+
+
+
+
+
+// var sampleUser = new User({
+//     email:'mgamatero@pogi.com',
+// })
+
+// sampleUser.save().then((doc)=>{
+//     console.log('User saved ',doc)
+// }, (e)=>{
+//     console.log('Error in saving user ',e)
+// })
