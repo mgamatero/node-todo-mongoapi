9ca63ad1c2426e884c825ca4e79e85f685921dd8
Michael Gamatero <mgamatero@gmail.com>
2018-10-24 11:06:26 -0700

    queries in playground and OBJECTID.isValid added

---
+++ playground/query.js
@@ -0,0 +1,27 @@
+var {mongoose} = require('./../server/db/mongoose')
+var {Todo} = require('./../server/models/todo')
+var {User} = require('./../server/models/user')
+
+var {ObjectID} = require('mongodb')
+
+var id = '5bd0ad4301186807ecc1ee4fzzzzz'
+
+if (!ObjectID.isValid(id)){
+    console.log('ID not valid (caught by ObjectID)')
+}
+// User.find({
+//     email:'mikepogs@aol.com'
+// }).then((doc)=>{
+//     console.log('User.find ----> ',doc)
+// },(e)=>{
+//     console.log(e)
+// })
+
+User.findById(id).then((doc)=>{
+    if(!doc){
+        return console.log('Unable to find user')
+    }
+    console.log('findById ----> ',JSON.stringify(doc,undefined,2))
+},(e)=>{
+    console.log(e)
+})
